f438f8aec38f3d294987dfc93e84cfba865ffe39
Clara <claraabmoura@gmail.com>
2025-08-28 18:55:23 -0300

    lo commit

---
+++ src/main/java/com/example/WebApp/HomeController.java
@@ -0,0 +1,14 @@
+package com.example.WebApp;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller 
+public class HomeController {
+
+	@RequestMapping("/clara") 
+	 public @ResponseBody String greetingClara() { 
+	  return "Hello, Clara!"; 
+	 }
+}
